c8388b65084a8c39b7b29d7e1e5fc517d86923e2
Samip Shrestha <samipshr@metropolia.fi>
2026-08-21 19:06:38 +0545

    Fix print statement syntax and variable name
    Corrected print statements to use valid syntax and fixed variable name.

--- Module_01/21_aug.py
+++ Module_01/21_aug.py
@@ -1,5 +1,5 @@
 print("hello")
 first_name= "samip"
 email= "luigi@gmail.com"
-print(f="you are {firest_name}")
-print(f="your mail is {email}")
+print(f"you are {firest_name}")
+print(f"your mail is {email}")
