6156b6d0c3bbca66ebb51fa1112f35db22d0a6e7
samipshr <samipshr@metropolia.fi>
2026-08-28 14:08:08 +0545

    Python_project01_starting_the_programming

---
+++ Project/Project_01.py
@@ -0,0 +1,9 @@
+#Game
+#program in the folder that asks for the player’s name and age, stores them in variables, and prints them to the console.
+print("Hello Player.")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"player name: {name}")
+print(f"player age: {age}")
