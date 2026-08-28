a7821469194bac7b7a00ab247580a0a4aa7df8b3
samipshr <samipshr@metropolia.fi>
2026-08-28 14:18:21 +0545

    Project1_renamed

--- Project/Project_01.py
+++
@@ -1,9 +0,0 @@
-#Game
-#program in the folder that asks for the player’s name and age, stores them in variables, and prints them to the console.
-print("Hello Player.")
-
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-
-print(f"player name: {name}")
-print(f"player age: {age}")
