608aa9506d6982d3e9c74b9dba516d66de111e72
samipshr <samipshr@metropolia.fi>
2026-08-31 18:02:53 +0545

    changing comment on project file

--- Project/MUFU.py
+++ Project/MUFU.py
@@ -1,5 +1,5 @@
 #Game
-#program in the folder that asks for the player’s name and age, stores them in variables, and prints them to the console.
+#project-01 : program that asks for the player’s name and age, stores them in variables, and prints them to the console.
 print("Hello Player.")
 
 name = input("Enter your name: ")
