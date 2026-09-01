a15036413880405c1cf17229a1f4b53fdb045068
samipshr <samipshr@metropolia.fi>
2026-09-01 11:46:13 +0545

    previous class correcting the code

--- Module_03/python_lesson.py
+++ Module_03/python_lesson.py
@@ -1,5 +1,5 @@
 #
-letter = input("enter a letter: ")
+letter = str(input("enter a letter: "))
 
 if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u": 
     print("this letter is a vowel")
